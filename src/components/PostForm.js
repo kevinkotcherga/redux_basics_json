4eb68ab7fa9ac9e98d23844ef422846cd8af064b
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-08 09:55:57 +0100

    ✨feat(react): création d'un post dans la db depuis react

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addPost } from "../actions/post.action";
+import { addPost, getPosts } from "../actions/post.action";
 
 const PostForm = () => {
   const [title, setTitle] = useState('');
@@ -8,7 +8,7 @@ const PostForm = () => {
   const user = useSelector((state) => state.userReducer);
   const dispatch = useDispatch();
 
-  const handleForm = (e) => {
+  const handleForm = async (e) => {
     e.preventDefault();
 
     if (title && content){
@@ -19,9 +19,10 @@ const PostForm = () => {
         likes: 0,
       };
 
-      dispatch(addPost(data));
+      await dispatch(addPost(data));
       setTitle('');
       setContent('');
+      dispatch(getPosts());
     }
   }
 
