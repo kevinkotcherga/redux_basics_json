bb27b6902353ee7e0dab48b2c912d0b45642c28a
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-08 12:31:37 +0100

    ✨feat(react): ajout de la fonction delete

--- src/actions/post.action.js
+++ src/actions/post.action.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const GET_POSTS = "GET_POSTS";
 export const ADD_POSTS = "ADD_POSTS";
+export const EDIT_POST = "EDIT_POST";
+export const DELETE_POST = "DELETE_POST";
 
 export const getPosts = () => {
   return (dispatch) => {
@@ -16,6 +18,33 @@ export const addPost = (data) => {
   return (dispatch) => {
     return axios.post('http://localhost:3000/posts', data).then((res) => {
       dispatch({ type: ADD_POSTS, payload: data})
+    })
+    .catch((err) => console.log(err))
+  };
+};
+
+export const editPost = (data) => {
+  return (dispatch) => {
+    return axios({
+      method: "put",
+      url: `http://localhost:3000/posts/${data.id}`,
+      data: { ...data }
+    })
+    .then((res) => {
+      dispatch({ type: EDIT_POST, payload: { ...data }})
+    })
+    .catch((err) => console.log(err))
+  };
+};
+
+export const deletePost = (postId) => {
+  return (dispatch) => {
+    return axios({
+      method: "delete",
+      url: `http://localhost:3000/posts/${postId}`,
+    })
+    .then((res) => {
+      dispatch({ type: DELETE_POST, payload: { postId }})
     })
     .catch((err) => console.log(err))
   };
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { deletePost, editPost } from "../actions/post.action";
 import Like from "./Like";
 import { isEmpty } from "./Utils";
 
 
 const Post = ({ post }) => {
   const [editToggle, setEditToggle] = useState(false);
-  const user = useSelector((state) => state.userReducer)
+  const [editContent, setEditContent] = useState(post.content)
+  const user = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleEdit = (element) => {
+    element.preventDefault();
+
+    const postData = {
+      title: post.title,
+      author: user[0].pseudo,
+      content: editContent,
+      likes: post.likes,
+      id: post.id
+    };
+    dispatch(editPost(postData))
+    setEditToggle(false);
+  }
 
   return (
     <div className="post">
       {!isEmpty(user[0]) && user[0].pseudo === post.author && (
         <div className="edit-delete">
           <img onClick={() => setEditToggle(!editToggle)} src="./icons/edit.svg" alt="edit"/>
-          <img src="./icons/delete.svg" alt="delete"/>
+          <img onClick={() => dispatch(deletePost(post.id))} src="./icons/delete.svg" alt="delete"/>
         </div>
       )}
       <h2>{post.title}</h2>
@@ -24,8 +41,8 @@ const Post = ({ post }) => {
       />
 
       {editToggle ? (
-        <form>
-          <textarea defaultValue={post.content}></textarea>
+        <form onSubmit={element => handleEdit(element)}>
+          <textarea defaultValue={post.content} onChange={element => setEditContent(element.target.value)}></textarea>
           <input type="submit" value="Valider modification" />
         </form>
       ) : (
