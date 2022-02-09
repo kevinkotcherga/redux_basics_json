8a9bad6e3b44204fad45162a661a4037c25deb22
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-09 13:57:54 +0100

    ✨feat(redux): remplacement des liens localhost:3000 par des liens heroku

--- src/actions/user.action.js
+++ src/actions/user.action.js
@@ -5,7 +5,7 @@ export const ADD_USER_LIKES = "ADD_USER_LIKES";
 
 export const getUser = () => {
   return (dispatch) => {
-    return axios.get('http://localhost:3000/users').then((res) => {
+    return axios.get('https://redux-app-crud-kotcherga.herokuapp.com/api/users').then((res) => {
       dispatch({ type: GET_USER, payload: res.data})
     })
     .catch((err) => console.log(err))
